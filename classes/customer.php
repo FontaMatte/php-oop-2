2356ccd4a65f4c5eb72a1496d283dea6b7851fd7
FontaMatte <matteo.fontanel@gmail.com>
2023-03-07 15:37:24 +0100

    Discount Trait Added

--- classes/customer.php
+++ classes/customer.php
@@ -3,14 +3,12 @@
 
         private $name;
         private $email;
-        private $isRegistered;
-        private $discount = 0;
+        private $paymentCard = false;
 
-        public function __construct($name, $email, $isRegistered) {
+        public function __construct($name, $email) {
 
             $this->name = $name;
             $this->email = $email;
-            $this->isRegistered = $isRegistered;
 
         }
 
@@ -22,35 +20,15 @@
             return $this->email;
         }
 
-        public function isRegistered() {
-            return $this->isRegistered;
+        public function getPaymentCard($paymentCard) {
+
+            $this->paymentCard = $paymentCard;
         }
 
-        public function getDiscount() {
-            return $this->discount;
+        public function setPaymentCard() {
+            return $this->paymentCard;
         }
 
-        public function setDiscount($discount) {
-            $this->discount = $discount;
-        }
-
-        public function discount($isRegistered) {
-            if ($isRegistered == true) {
-                return $this->discount = 20;
-            }
-            else {
-                return $this->discount;
-            }
-        }
     }
-
-    $customer1 = new Customer('piero','pierogmail.com', false);
-
-    var_dump($customer1);
-
-    echo '</br>';
-
-    var_dump($customer1->discount($customer1->isRegistered()));
-
 
 ?>
